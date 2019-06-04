dcfb1d16e6c2ab84de042d653ad3acbd82530213
hodayaBerdugo <hodaya_dr0@walla.com>
2019-06-04 21:20:12 +0300

    Initial commit

---
+++ Model/assetModel.go
@@ -0,0 +1,20 @@
+package Model
+
+type Asset struct {
+	location string
+	meters  int
+	rooms int
+	floor int
+	price int
+
+
+//	func(i *Asset) String() string {
+//	return strconv.itoa(i)
+//}
+}
+
+//
+//type Asset struct {
+//	name string
+//	age  int
+//}
